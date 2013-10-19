079c9b8d95f99423c70dc59f968ad68f4bbdfac1
dillius <dillius@gmail.com>
2013-10-18 20:14:11 -0400

    Interacting with HTTP services

--- auto.go
+++ auto.go
@@ -3,32 +3,52 @@ package main
 import (
        "fmt"
        "os"
-       "strconv"
+       "net/http"
+       "net/url"
+       "io/ioutil"
 )
 
-func updateUser(amt int) {
-     user := os.Getenv("PTS_USER")
-     fmt.Println("Updating " + user)
-     fmt.Println("Amount " + strconv.Itoa(amt))
+const api_url = "http://www.peaktolerablestupidity.com/api/entry"
+
+func getUser() {
+     resp, _ := http.Get(api_url + "/" + os.Getenv("PTS_USER"))
+     defer resp.Body.Close()
+     contents, _ := ioutil.ReadAll(resp.Body)
+     fmt.Printf("%s\n", string(contents))
+
 }
 
-func bihourUpdate() {
-     rate := os.Getenv("PTS_BIHOUR_RATE")
+func updateUser(amt string) {
+     user := os.Getenv("PTS_USER")
+     fmt.Println("Updating " + user)
+     fmt.Println("Amount " + amt)
+     values := make(url.Values)
+     values.Set("user", user)
+     values.Set("level", amt)
+
+     resp, _ := http.PostForm(api_url, values)
+     defer resp.Body.Close()
+}
+
+func autoUpdate() {
+     rate := os.Getenv("PTS_AUTO_RATE")
      if rate == "" {
         rate = "0"
      }
-     amt, err := strconv.Atoi(rate)
+     /*amt, err := strconv.Atoi(rate)
      if err != nil {
         // handle error
         fmt.Println(err)
         os.Exit(2)
-     }
-     fmt.Println(amt)
-     updateUser(amt)
+     }*/
+     updateUser("+" + rate)
 }
 
 func main() {
      fmt.Printf("Hello, world!\n")
-     updateUser(1)
-     bihourUpdate()
+     getUser()
+     updateUser("10")
+     getUser()
+     autoUpdate()
+     getUser()
 }
